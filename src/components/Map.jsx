c4486ca723b7492ba4bcf912e73c0b3a44f10d37
Wendy Zhu <wendyy.zhu@hotmail.com>
2023-09-11 10:12:38 +1000

    load all station markers on google map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,14 +9,41 @@ export default function Map() {
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY 
     })
 
+    const [stations, setStations] = useState([])
+
+    useEffect(() => {
+        fetch('/api/stations/all')
+            .then(res => res.json())
+            .then(stations => setStations(stations))
+    },[])
+
     return isLoaded ? (
-        <GoogleMap
-            zoom={5}
-            center={center}
-            mapContainerClassName='google-map'
-        >
-            <MarkerF position={center}>
-            </MarkerF>
-        </GoogleMap>
+        <div className='map'>
+            {/* <label>State</label>
+            <select>
+                <option value="All">All</option>
+                <option value="NSW">New South Wales</option>
+                <option value="QLD">Queensland</option>
+                <option value="SA">South Australia</option>
+                <option value="VIC">Victoria</option>
+            </select> */}
+
+            <GoogleMap
+                zoom={5}
+                center={center}
+                mapContainerClassName='google-map'
+            >
+                
+                {stations.map(station => {
+                    return <MarkerF
+                                key={station.id}
+                                position={{ lat: station.latitude, lng: station.longitude }}>
+                           </MarkerF>
+                })}
+
+                <MarkerF position={center}>
+                </MarkerF>
+            </GoogleMap>
+        </div>
     ) : <></>
 }
